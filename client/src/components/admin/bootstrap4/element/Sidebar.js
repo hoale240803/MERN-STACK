c853ba53edc4cd29c2dafa09a9b074b28caeb8e1
lehoa240119999 <hoale240803@gmail.com>
2021-05-15 15:57:29 +0700

    udpate button, move assets to src folder

--- client/src/components/admin/bootstrap4/element/Sidebar.js
+++ client/src/components/admin/bootstrap4/element/Sidebar.js
@@ -1,16 +1,51 @@
 import React from "react";
 
+import logo from "../../../../assets/images/browser/chrome.png";
 const Sidebar = () => {
   return (
     <div class="vue__block vue__sidebar">
-      <div class="vue__logo bg-info">vue__logo</div>
+      <div class="vue__logo">
+        <div class="logo__name">
+          <img src={logo} alt="logo" className="vue__logo__img" />
+          <h3>HoaL</h3>
+        </div>
+        <div className="btn__zoom">
+          <i class="far fa-dot-circle"></i>
+        </div>
+      </div>
       <div class="dashboard__option">
         <div class="list-group">
-          <a
-            href="#"
-            class="list-group-item list-group-item-action active vue__btn"
-          >
-            Active item
+          <a href="#" class="list-group-item list-group-item-action vue__btn">
+            <div class="vue__icon__text__btn__arrow">
+              <div class="vue__icon__text">
+                <div class="vue__btn__icon__home vue__btn__icon">
+                  <i class="fas fa-home"></i>
+                </div>
+                <div class="vue__btn__text vue__btn__text__home">Dashboard</div>
+              </div>
+              <div class="vue__btn__arrow">
+                <div class="vue__noti__number">2</div>
+                <i class="fas fa-angle-down"></i>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+      <div class="dashboard__option">
+        <div class="list-group">
+          <a href="#" class="list-group-item list-group-item-action vue__btn">
+            <div class="vue__icon__text__btn__arrow">
+              <div class="vue__icon__text">
+                <div class="vue__btn__icon">
+                  <i class="far fa-circle"></i>
+                </div>
+                <div class="vue__btn__text">Menu Items</div>
+              </div>
+              <div class="vue__btn__arrow">
+                <div class="vue__noti__number">2</div>
+                <i class="fas fa-angle-down"></i>
+              </div>
+            </div>
           </a>
         </div>
       </div>
@@ -21,14 +56,16 @@ const Sidebar = () => {
             class="list-group-item list-group-item-action vue__btn"
             id="first__item"
           >
-            <div class="vue__icon__text">
-              <div class="vue__btn__icon">
-                <i class="fab fa-bitcoin"></i>
+            <div class="vue__icon__text__btn__arrow">
+              <div class="vue__icon__text">
+                <div class="vue__btn__icon">
+                  <i class="far fa-circle"></i>
+                </div>
+                <div class="vue__btn__text">Right</div>
               </div>
-              <div class="vue__btn__text">Dashboard</div>
-            </div>
-            <div class="vue__btn__arrow">
-              <i class="fas fa-arrow-right"></i>
+              <div class="vue__btn__arrow">
+                <i class="fas fa-chevron-right"></i>
+              </div>
             </div>
           </a>
           <a
@@ -39,15 +76,27 @@ const Sidebar = () => {
           "
             id="second__item"
           >
-            <div class="vue__icon__text">
-              <div class="vue__btn__icon">icon</div>
-              <div class="vue__btn__text">text</div>
+            <div class="vue__icon__text__btn__arrow">
+              <div class="vue__icon__text">
+                <div class="vue__btn__icon">icon</div>
+                <div class="vue__btn__text">text</div>
+              </div>
+              <div class="vue__btn__arrow">arrow</div>
             </div>
-            <div class="vue__btn__arrow">arrow</div>
           </a>
-          <a href="" class="vue__box vue__btn">
-            {" "}
-            box{" "}
+          <a href="#" class="list-group-item list-group-item-action vue__btn">
+            <div class="vue__icon__text__btn__arrow">
+              <div class="vue__icon__text">
+                <div class="vue__btn__icon">
+                  <i class="fas fa-home"></i>
+                </div>
+                <div class="vue__btn__text ">Dashboard</div>
+              </div>
+              <div class="vue__btn__arrow">
+                <div class="vue__noti__text">New</div>
+                <i class="fas fa-angle-down"></i>
+              </div>
+            </div>
           </a>
         </div>
       </div>
