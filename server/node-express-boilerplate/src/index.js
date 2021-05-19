e0618293d6a30d9c2c519bd9f8ed7799b1c066ba
lehoa240119999 <hoale240803@gmail.com>
2021-05-19 13:25:54 +0700

    remove husky, update docker-compose, update package.json

--- server/node-express-boilerplate/src/index.js
+++ server/node-express-boilerplate/src/index.js
@@ -7,7 +7,7 @@ let server;
 mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
   logger.info('Connected to MongoDB');
   server = app.listen(config.port, () => {
-    logger.info(`Listening to port ${config.port}`);
+    logger.info(`Listening to port ${config.port}, full link: http://localhost:5000/v1/docs/ `);
   });
 });
 
